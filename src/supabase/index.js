bc7b4901735954b0381c94c8e1ba50aaca09d5b7
maksimka241e <97219359+maksimka241e@users.noreply.github.com>
2024-09-12 17:01:55 +0300

    Update index.js

--- src/supabase/index.js
+++ src/supabase/index.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  'https://imxkxwqdtnvmbyevaxaz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlteGt4d3FkdG52bWJ5ZXZheGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk3MTE1ODMsImV4cCI6MjAzNTI4NzU4M30.p2nuAUq-17cP3QUmvmeqdFU24B4YjClXuigNIu8wa0Q',
-)
+const apiKey = process.env.KEY_API
+const urlApi = process.env.URL_API
+const supabase = createClient(`${urlApi}`, `${apiKey}`)
 
 export default supabase
