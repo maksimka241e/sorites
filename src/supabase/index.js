0fde0b4cee608d0e5758646d782dfccbcfc30094
maksimka241e <97219359+maksimka241e@users.noreply.github.com>
2024-09-19 18:04:04 +0300

    Update index.js

--- src/supabase/index.js
+++ src/supabase/index.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const apiKey = process.env.KEY_API
 const urlApi = process.env.URL_API
-const supabase = createClient(`${urlApi}`, `${apiKey}`)
+const supabase = createClient(urlApi, apiKey)
 
 export default supabase
